55af19daea106780addb2a402e67fd074d9b051a
Patrick Schwemmer <schwemme@cuda10.informatik.uni-bonn.de>
2023-03-09 23:15:27 +0100

    RNN UNet, RNN needs implementation

--- src/modules.py
+++ src/modules.py
@@ -9,7 +9,6 @@ class ConvWithNorm(nn.Module):
 
     def __init__(
         self,
-        dim: int,
         in_channels: int,
         out_channels: int,
         kernel_size: Union[int, tuple[int, ...]],
@@ -18,15 +17,14 @@ class ConvWithNorm(nn.Module):
         batch_norm: bool = True,
     ) -> None:
         super(ConvWithNorm, self).__init__()
-        self.conv = ConvNd(
-            dim=dim,
+        self.conv = nn.Conv2d(
             in_channels=in_channels,
             out_channels=out_channels,
             kernel_size=kernel_size,
             padding=padding,
             stride=stride,
         )
-        self.norm = BatchNormNd(dim, out_channels) if batch_norm else nn.Identity()
+        self.norm = nn.BatchNorm2d(out_channels) if batch_norm else nn.Identity()
         self.activation = nn.ReLU(inplace=True)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -41,18 +39,15 @@ class ConvBlock(nn.Sequential):
 
     def __init__(
         self,
-        dim: int,
         channels: list[int],
         batch_norm: bool = True,
     ) -> None:
         """Args:
         `channels` : Number of channels in each convolutional layer.
-        `dim` : Dimensionality of the input tensor.
         """
         super(ConvBlock, self).__init__(
             *[
                 ConvWithNorm(
-                    dim=dim,
                     in_channels=in_channels,
                     out_channels=out_channels,
                     kernel_size=3,
@@ -69,24 +64,21 @@ class UpsampleBlock(nn.Module):
 
     def __init__(
         self,
-        dim: int,
         channels: list[int],
         batch_norm: bool = True,
         cut_channels_on_upsample: bool = False,
     ) -> None:
         """Args:
         `channels` : Number of channels in each convolutional layer.
-        `dim` : Dimensionality of the input tensor.
         """
         super(UpsampleBlock, self).__init__()
-        self.upsample = ConvTransposeNd(
-            dim=dim,
+        self.upsample = nn.ConvTranspose2d(
             in_channels=channels[0],
             out_channels=channels[0] // 2 if cut_channels_on_upsample else channels[0],
             kernel_size=2,
             stride=2,
         )
-        self.conv_block = ConvBlock(dim, channels, batch_norm)
+        self.conv_block = ConvBlock(channels, batch_norm)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.upsample(x)
@@ -99,56 +91,29 @@ class DownsampleBlock(nn.Module):
 
     def __init__(
         self,
-        dim: int,
         channels: list[int],
         use_pooling: bool = False,
         batch_norm: bool = True,
     ) -> None:
         """Args:
         `channels` : Number of channels in each convolutional layer.
-        `dim` : Dimensionality of the input tensor.
         """
         super(DownsampleBlock, self).__init__()
         if use_pooling:
-            self.downsampling = MaxPoolNd(dim=dim, kernel_size=2, stride=2)
+            self.downsampling = nn.MaxPool2d(kernel_size=2, stride=2)
         else:
-            self.downsampling = ConvNd(
-                dim=dim,
+            self.downsampling = nn.Conv2d(
                 in_channels=channels[0],
                 out_channels=channels[0],
                 kernel_size=2,
                 stride=2,
             )
-        self.conv_block = ConvBlock(dim, channels, batch_norm)
+        self.conv_block = ConvBlock(channels, batch_norm)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.downsampling(x)
         x = self.conv_block(x)
         return x
-
-
-def ConvNd(dim: int, *args, **kwargs) -> Union[nn.Conv1d, nn.Conv2d, nn.Conv3d]:
-    assert dim > 0 and dim < 4, "Attribute 'dim' has to be >0 and <4."
-    cls = getattr(nn, f"Conv{dim}d")
-    return cls(*args, **kwargs)
-
-
-def ConvTransposeNd(dim: int, *args, **kwargs) -> Union[nn.ConvTranspose1d, nn.ConvTranspose2d, nn.ConvTranspose3d]:
-    assert dim > 0 and dim < 4, "Attribute 'dim' has to be >0 and <4."
-    cls = getattr(nn, f"ConvTranspose{dim}d")
-    return cls(*args, **kwargs)
-
-
-def BatchNormNd(dim: int, *args, **kwargs) -> Union[nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d]:
-    assert dim > 0 and dim < 4, "Attribute 'dim' has to be >0 and <4."
-    cls = getattr(nn, f"BatchNorm{dim}d")
-    return cls(*args, **kwargs)
-
-
-def MaxPoolNd(dim: int, *args, **kwargs) -> Union[nn.MaxPool1d, nn.MaxPool2d, nn.MaxPool3d]:
-    assert dim > 0 and dim < 4, "Attribute 'dim' has to be >0 and <4."
-    cls = getattr(nn, f"MaxPool{dim}d")
-    return cls(*args, **kwargs)
 
 
 def init_weights(m: nn.Module) -> None:
